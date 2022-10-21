467f3b7dc182d78f8a90b869fd4f01e826f972e5
haclesrocha200@gmail.com <71661551+hacles100@users.noreply.github.com>
2022-10-20 23:20:16 -0100

    first-commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const hoursHand = document.querySelector('.hand.hours')
+const minutesHand = document.querySelector('.hand.minutes')
+const secondsHand = document.querySelector('.hand.seconds')
+
+const setRotation = (element, rotationPercentage) => {
+    element.style.setProperty("--rotation", rotationPercentage * 360);
+}
+
+
+const setClock = () => {
+    const currentDate = new Date();
+
+    const secondsPercentage = currentDate.getSeconds() / 60;
+    const minutesPercentage = currentDate.getMinutes() / 60;
+    const hoursPercentage = currentDate.getHours() / 12;  
+
+    setRotation(secondsHand, secondsPercentage);
+    setRotation(minutesHand, minutesPercentage);
+    setRotation(hoursHand, hoursPercentage);
+}
+
+setClock();
+
+setInterval(setClock, 1000);
